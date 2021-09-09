0f76c62d0288be3c8fc98e9db4f58382952cd7c5
Kadir Erman <45540321+kadirermantr@users.noreply.github.com>
2021-09-10 01:51:52 +0300

    crud fonksiyonları eklendi

--- post.class.php
+++ post.class.php
@@ -12,5 +12,64 @@ class Post extends DB {
     }
   }
 
+    public function getCategoryList () {
+        $categories = $this->connect()->query("SELECT * FROM category");
 
+        foreach($categories->fetchAll(PDO::FETCH_ASSOC) as $category) {
+            echo '<option value="'.$category["category_id"].'">'.$category["name"].'</option>';
+        }
+    }
+
+    public function create($title, $content, $category_id, $image, $author, $source) {
+        $addQuery = "INSERT INTO post (title, content, category_id, post_image, author, source) VALUES('$title', '$content', $category_id, '$image', '$author', '$source')";
+        $this->connect()->query($addQuery);
+    }
+
+    public function update($post_id, $title, $content, $category_id, $post_image, $author, $source) {
+        if ($post_image["size"] > 0) {
+            $uploads_dir = 'img/posts';
+            @$tmp_name = $post_image["tmp_name"];
+            @$name = $post_image["name"];
+            $refimgyol=$uploads_dir."/".$name;
+            @move_uploaded_file($tmp_name, "$uploads_dir/$name");
+
+            $edit=$this->connect()->prepare("UPDATE post SET
+            post_image=:post_image
+            WHERE post_id={$post_id}");
+            $update=$edit->execute(array(
+                'post_image' => $refimgyol,
+            ));
+            $removeImage=$_POST['oldImage'];
+            unlink("$removeImage");
+        }
+
+        //sql sorgusu imageyi komut olarak tanımlıyor adını değiştir
+
+        $edit=$this->connect()->prepare("UPDATE post SET
+            title=:title,
+            content=:content,
+            category_id=:category_id,
+            author=:author,
+            source=:source
+            WHERE post_id={$post_id}");
+
+        $update=$edit->execute(array(
+            'title' => $title,
+            'content' => $content,
+            'category_id' => $category_id,
+            'author' => $author,
+            'source' => $source,
+        ));
+    }
+
+    public function delete() {
+        $post_id = $_GET["post_id"];
+        $remove = $this->connect()->prepare("DELETE FROM post WHERE post_id=:id");
+
+        $delete = $remove->execute(array(
+            'id'=> $post_id
+        ));
+
+        //unlink("$post_image");
+    }
 }
