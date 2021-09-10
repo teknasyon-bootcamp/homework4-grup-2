73c7f3809f6070305ab3722d47da2513721208cd
Hkaan <63049706+nightman413@users.noreply.github.com>
2021-09-11 01:43:25 +0300

    Add files via upload

--- index.php
+++ index.php
@@ -1,8 +1,18 @@
-<?php
+<?php include "layouts/header.php" ?>
 
-require_once 'post.class.php';
+    
+    <?php
+    if(!isset($_GET["post"])){
+        echo "<div class='posts'>";
+            $deneme = new Post;
+            $deneme->getPostList();
+        echo "</div>";
+    }elseif(isset($_GET["post"])){
+        echo "<div class='single_post'>";
+            $single_post = new Post;
+            $single_post->getPost();
+        echo "</div>";
+    }
+    ?>
 
-
-
-$deneme = new Post;
-$deneme->getPostList();
+<?php include "layouts/footer.php" ?>
---
+++ layouts/header.php
@@ -0,0 +1,36 @@
+<?php
+ error_reporting(1);
+require_once 'post.class.php'; ?>
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <link rel="stylesheet" href="css/style.css">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap@5.1.0/dist/css/bootstrap.min.css" integrity="sha384-KyZXEAg3QhqLMpG8r+8fhAXLRk2vvoC2f3B09zVXn8CA5QIVfZOJ3BCsw2P0p/We" crossorigin="anonymous">
+    <link rel="preconnect" href="https://fonts.googleapis.com">
+    <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
+    <link href="https://fonts.googleapis.com/css2?family=PT+Sans&display=swap" rel="stylesheet">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+<svg xmlns="http://www.w3.org/2000/svg" style="display: none;">
+  <symbol id="check-circle-fill" fill="currentColor" viewBox="0 0 16 16">
+    <path d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zm-3.97-3.03a.75.75 0 0 0-1.08.022L7.477 9.417 5.384 7.323a.75.75 0 0 0-1.06 1.06L6.97 11.03a.75.75 0 0 0 1.079-.02l3.992-4.99a.75.75 0 0 0-.01-1.05z"/>
+  </symbol>
+</svg>
+    <div class="sidebar">
+        <img src="img/logo.png" alt="LOGO">
+        <ul>
+            <li>
+                <a href="index.php">Makaleler</a>
+            </li>
+            <li>
+                <a href="manage.php?action=create">Yazı Ekle</a>
+            </li>
+            <li>
+                <a href="manage.php">Admin</a>
+            </li>
+        </ul>
+    </div>
